06bd422fb87b6c541d214bbc3ca17fe461a47a4b
Gaurav <singh1411gaurav@gmail.com>
2023-01-18 15:44:14 +0000

    modified

--- src/main/java/driver/BrowserDriver.java
+++ src/main/java/driver/BrowserDriver.java
@@ -7,6 +7,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BrowserDriver {
 	
 	public void load() {
+		System.out.println("inside WebDriver Manager");
 		WebDriverManager.chromedriver().setup();
 	
 	}
